75afb94613c6e16e89021c3820bbbce436e57442
Santhosh-dheenan <santhoshdheenanvpl@gmail.com>
2025-05-08 15:22:09 +0530

    test

--- billing-type-/Sample.java
+++ billing-type-/Sample.java
@@ -1,26 +1,34 @@
 import java.util.*;
-class Sample{
-    static void myMethod(String name,int reate){
-        System.out.println("prodect : "+name);
-        System.out.println("reate : "+reate);
-        Scanner scan = new Scanner(System.in);
-        System.out.print("how many whant you Enter : ");
-        int pack = scan.nextInt();
-        int Total = reate * pack;
-        System.out.println("Total = "+Total);
-    }
-    public static void main(String[]args){
-        System.out.print("Now only one menu Enter the menu number 5 : ");
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        if (5 == num){
-            myMethod("bingo cips",10);
-            
-        }
-        else{
-            System.out.println("not avalible prodecte Data");
-        }
-        System.out.println("Well Come our Shop");
 
+class Sample {
+    static void myMethod(String name, int rate) {
+        System.out.println("Product: " + name);
+        System.out.println("Rate: " + rate);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many do you want to buy: ");
+        int quantity = scan.nextInt();
+
+        int total = rate * quantity;
+        System.out.println("Total = " + total);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1. Bingo");
+        System.out.println("2. Lollipop");
+        System.out.print("Enter the menu number (only one): ");
+        int num = scan.nextInt();
+
+        if (num == 1) {
+            myMethod("Bingo Chips", 10);
+        } else if (num == 2) {
+            myMethod("Lollipop", 5);
+        } else {
+            System.out.println("Invalid option selected.");
+        }
+
+        System.out.println("Welcome to our shop!");
     }
 }
